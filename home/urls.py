eaa2e7139c8c292e111de1819ed2b281f9bda201
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2023-09-17 03:45:22 -0700

    WIP

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
 	path('registration', views.registration, name='registration'),
 	path('login', views.login, name='login'),
 	path('createevent', views.createevent, name='createevent'),
-    path('events', views.eventshow, name='events'),
-    path('contacts', views.contacts, name='contacts'),
+    path('wedding', views.wedding, name='wedding'),
     path('birthday', views.birthday, name='birthday'),
+    path('concert', views.concert, name='concert'),
+    path('corporate', views.corporate, name='corporate'),
 ]
